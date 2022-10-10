9e73fd3462c098b42f35a8ab7e5abbfc557ae587
BrianMilmanXansium <52654865+uxylab@users.noreply.github.com>
2022-10-10 20:02:35 +0100

    update main

--- app/routes.js
+++ app/routes.js
@@ -2,15 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 
-router.post(`/question`, function (req, res) {
-    const accountType = req.session.data['account-type'];
-  
-    if (accountType === 'Personal') {
-        res.redirect(`/v${verNum}/create-account/name`);
-    } else {
-        res.redirect(`/v${verNum}/create-account/name`);
-    }
-  });
+
 
 // Add your routes here - above the module.exports line
 
